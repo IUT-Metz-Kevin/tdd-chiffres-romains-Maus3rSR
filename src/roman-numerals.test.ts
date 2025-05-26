d22012057844394ce53a902b300127779a876592
Kevin Unfricht <p02865@univ-lorraine.fr>
2025-05-26 14:01:59 +0200

    RED

--- src/roman-numerals.test.ts
+++ src/roman-numerals.test.ts
@@ -4,6 +4,6 @@ class RomanNumeral {
   of(value: string): number {}
 }
 
-test.each(['I', 1])('%s should become %s', (input, expected) => {
-  expect(input).toBe(expected);
+test.each([['I', 1]])('%s should become %s', (input, expected) => {
+  expect(new RomanNumeral().of(input)).toBe(expected);
 });
