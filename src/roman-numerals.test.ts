fc2468dbd4c58912e3065f6d34c4d54db5a1a4ce
Kevin Unfricht <p02865@univ-lorraine.fr>
2025-05-26 14:02:18 +0200

    GREEN

--- src/roman-numerals.test.ts
+++ src/roman-numerals.test.ts
@@ -1,7 +1,9 @@
 import { test, expect } from 'vitest';
 
 class RomanNumeral {
-  of(value: string): number {}
+  of(value: string): number {
+    if (value === 'I') return 1;
+  }
 }
 
 test.each([['I', 1]])('%s should become %s', (input, expected) => {
