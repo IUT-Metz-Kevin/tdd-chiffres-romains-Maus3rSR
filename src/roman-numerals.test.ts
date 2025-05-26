39090bee8f4e9a1066336e04026b2a08bbd235ae
Kevin Unfricht <p02865@univ-lorraine.fr>
2025-05-26 14:13:31 +0200

    GREEN

--- src/roman-numerals.test.ts
+++ src/roman-numerals.test.ts
@@ -8,6 +8,10 @@ class RomanNumeral {
       if (value[index] === 'I') resultat += 1;
       if (value[index] === 'V') resultat += 5;
       if (value[index] === 'X') resultat += 10;
+      if (value[index] === 'L') resultat += 50;
+      if (value[index] === 'C') resultat += 100;
+      if (value[index] === 'D') resultat += 500;
+      if (value[index] === 'M') resultat += 1000;
       index++;
     }
 
