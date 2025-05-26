86df11e77600ca6b18d7086364be062d1c1b2998
Kevin Unfricht <p02865@univ-lorraine.fr>
2025-05-26 14:00:32 +0200

    RED

--- src/roman-numerals.test.ts
+++ src/roman-numerals.test.ts
@@ -1,1 +1,9 @@
-// Write your test here
+import { test, expect } from 'vitest';
+
+class RomanNumeral {
+  of(value: string): number {}
+}
+
+test.each(['I', 1])('%s should become %s', (input, expected) => {
+  expect(input).toBe(expected);
+});
